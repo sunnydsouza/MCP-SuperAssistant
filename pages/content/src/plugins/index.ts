83d87b5233c6c79386db460b150bb4095125e88f
Sunny D <530328+sunnydsouza@users.noreply.github.com>
2026-09-09 17:45:52 +0530

    feat: expose Microsoft 365 Copilot adapter

--- pages/content/src/plugins/index.ts
+++ pages/content/src/plugins/index.ts
@@ -16,6 +16,7 @@ export { T3ChatAdapter } from './adapters/t3chat.adapter';
 export { AIStudioAdapter } from './adapters/aistudio.adapter';
 export { MistralAdapter } from './adapters/mistral.adapter';
 export { GitHubCopilotAdapter } from './adapters/ghcopilot.adapter';
+export { Microsoft365CopilotAdapter } from './adapters/m365copilot.adapter';
 export { SidebarPlugin } from './sidebar.plugin';
 export { createPluginContext } from './plugin-context';
 
